d9e99013eba18f5a936a705cf520ac1558f68f32
lopezj18 <juan@juanlopezcreative.com>
2014-01-27 13:37:29 -0500

    Here are the starting files of the Printscape Ordering Management System

---
+++ header.php
@@ -0,0 +1,30 @@
+<?php
+
+?>
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Printscape</title>
+<link rel="stylesheet" type="text/css" href="css/reset.css">
+<link rel="stylesheet" type="text/css" href="css/clearfix.css">
+<link rel="stylesheet" type="text/css" href="css/styles.css">
+</head>
+
+
+<body>
+
+<div class="wrapper">
+<header>
+<div><img src="images/logo.png"><p>Ordering Management System</p></div>
+
+<div>
+<!--this line of code below displays "welcome:"user's name""-->
+<?php /*?><?php
+if(isset($_SESSION['logged_in'])){
+ echo $welcome;echo $_SESSION['username'];
+}
+?><?php */?>
+</header>
+
+
