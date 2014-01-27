439eae395a85a3f3da9a4f47eca16ff1ad696849
lopezj18 <juan@juanlopezcreative.com>
2014-01-27 16:46:59 -0500

    more changes

---
+++ admin-logged-in.php
@@ -0,0 +1,27 @@
+
+<?php
+
+session_start();
+include("header.php");
+?>
+
+<style type="text/css">
+.tftable {font-size:12px;color:#333333;width:100%;border-width: 1px;border-color: #a9a9a9;border-collapse: collapse; margin-top:20px;}
+.tftable th {font-size:12px;background-color:#c1d72e;border-width: 1px;padding: 8px;border-style: solid;border-color: #a9a9a9;text-align:left;}
+.tftable tr {background-color:#ffffff;}
+.tftable td {font-size:12px;border-width: 1px;padding: 8px;border-style: solid;border-color: #a9a9a9;}
+.tftable tr:hover {background-color:#f3fcb9;}
+</style>
+
+<h1 class="h1_titles">Recent Orders</h1>
+<table class="tftable" border="1">
+<tr><th>Job Id</th><th>Type</th><th>Due Date</th><th>Date Submited</th><th>Status</th><th>Download File</th><th>Details</th></tr>
+<tr><td>Cell:1</td><td>Cell:2</td><td>Cell:3</td><td>Cell:4</td><td>Cell:5</td><td>Cell:6</td><td>Cell:6</td>
+</table>
+
+
+<?php
+include("footer.php");
+?>
+
+
